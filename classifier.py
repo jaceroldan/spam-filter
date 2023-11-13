f8dce65f32072e5360b667eeace4d20cc4ea4c6c
Jace Roldan <jaceroldan16@gmail.com>
2023-11-13 10:31:47 +0800

    Retrieve only vocabulary words which match [a-zA-Z]*

--- classifier.py
+++ classifier.py
@@ -21,19 +21,41 @@ ham_vocabulary = {}
 with open('hamvectors.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',')
     for line_path in ham_line_paths:
-        with open(os.path.join(os.getcwd(), re.sub(r'ham \.\.\/', '', ham_line_paths[0])).strip(), 'r') as hamfile:
+        with open(os.path.join(os.getcwd(), re.sub(r'ham \.\.\/', '', line_path)).strip(), 'rb') as hamfile:
             ham_file_lines = []
             file_vocab = {}
             for hamfile_line in hamfile.readlines():
-                split_words = hamfile_line.split()
+                try:
+                    split_words = re.findall(r'^[A-Za-z]*$', hamfile_line.decode('utf-8'))
+                except UnicodeDecodeError:
+                    continue
+                # split_words = hamfile_line.split()
                 for w in split_words:
-                    if ham_vocabulary
+                    # print(w.lower())
+                    if ham_vocabulary.get(w.lower(), None):
+                        ham_vocabulary[w.lower()] += 1
+                    else:
+                        ham_vocabulary[w.lower()] = 1
+            # print(len(ham_vocabulary.values()))
+            # print(line_path)
+
+# print(len(ham_vocabulary.values()))
+# print(ham_vocabulary.items())
+# print(sorted(ham_vocabulary.items(), key=lambda v: v[1], reverse=True)[0])
+
+# print(len(ham_vocabulary.values()))
+# print(ham_vocabulary['newsgroups:'])
+# print(ham_vocabulary)
+
+# with open(os.path.join(os.getcwd(), 'data/000/000')) as hamfile:
+#     for hamfile_line in hamfile.readlines():
+#         print(hamfile_line.split())
 
 
-vocabulary_spam = {}
-with open('spamvectors.csv', 'w') as csvfile:
-    writer = csv.writer(csvfile, delimiter=',')
-    print(os.path.join(os.getcwd(), re.sub(r'spam \.\.\/', '', spam_lines[0])))
+# vocabulary_spam = {}
+# with open('spamvectors.csv', 'w') as csvfile:
+#     writer = csv.writer(csvfile, delimiter=',')
+#     print(os.path.join(os.getcwd(), re.sub(r'spam \.\.\/', '', spam_line_paths[0])))
 
 
 # Start reading all documents
