b408ad9deb9a8fc1f2b9387a655d4cc2cb8e8cdb
Jace Roldan <jaceroldan16@gmail.com>
2023-11-23 18:18:00 +0800

    Add notebook

--- file_reader.py
+++ file_reader.py
@@ -7,6 +7,7 @@ import random
 # DOING: Build dictionary of 
 # TODO: Shuffle to get 70% training, 30% test
 # Get labels as to which files are for training, and which are for test
+master_vocabulary = []
 ham_line_paths = []
 spam_line_paths = []
 with open('./labels', 'r') as file:
@@ -18,7 +19,7 @@ with open('./labels', 'r') as file:
     # print(len(ham_lines) + len(spam_lines))
 
 
-def write_word_vectors(filename, line_paths, classification):
+def write_word_vectors(filename, line_paths, classification, master_vocabulary):
     full_vocabulary = {}
     full_file_vocabs = []
     with open(filename, 'w', newline='') as csvfile:
@@ -44,6 +45,7 @@ def write_word_vectors(filename, line_paths, classification):
                             file_vocab[w.lower()] += 1
                         else:
                             file_vocab[w.lower()] = 1
+                        master_vocabulary.append(w)
                 full_file_vocabs.append(file_vocab)
 
         full_vocabulary_wordset = sorted(full_vocabulary.keys())
@@ -58,11 +60,11 @@ def write_word_vectors(filename, line_paths, classification):
             rows_to_add.append(row)
         writer.writerows(rows_to_add)
     
-    return full_vocabulary, full_file_vocabs
+    return full_vocabulary, full_file_vocabs, list(set(master_vocabulary))
 
 
-ham_vocabulary, ham_file_vocabs = write_word_vectors('hamvectors.csv', ham_line_paths, 'ham')
-spam_vocabulary, spam_file_vocabs = write_word_vectors('spamvectors.csv', spam_line_paths, 'spam')
+ham_vocabulary, ham_file_vocabs, master_vocabulary = write_word_vectors('hamvectors.csv', ham_line_paths, 'ham', master_vocabulary)
+spam_vocabulary, spam_file_vocabs, master_vocabulary = write_word_vectors('spamvectors.csv', spam_line_paths, 'spam', master_vocabulary)
 
 print(len(ham_vocabulary.values()))
 print(len(spam_vocabulary.values()))
@@ -93,4 +95,61 @@ spam_training, spam_test = take_split(spam_full, 70)
 print(len(spam_file_vocabs))
 print(len(spam_training), len(spam_test))
 
+# TODO: What if the spam and ham classes are not evenly split?
+# Unchecked
+print('priors: ')
+print('ham class: ', len(ham_training), '/', len(ham_training) + len(spam_training))
+print('spam class: ', len(spam_training),  ' / ', len(ham_training) + len(spam_training))
 
+# We only care about how many times this word occurred across different document Ds
+rebuilt_ham_vocabulary_probabilities = {}
+for item in ham_training:
+    for word in item:
+        if rebuilt_ham_vocabulary_probabilities.get(word, None):
+            rebuilt_ham_vocabulary_probabilities[word] += 1
+        else:
+            rebuilt_ham_vocabulary_probabilities[word] = 1
+
+for key in rebuilt_ham_vocabulary_probabilities.keys():
+    rebuilt_ham_vocabulary_probabilities[key] = (rebuilt_ham_vocabulary_probabilities[key] + 1) / (len(ham_training) + len(master_vocabulary))
+
+
+
+rebuilt_spam_vocabulary_probabilities = {}
+for item in spam_training:
+    for word in item:
+        if rebuilt_spam_vocabulary_probabilities.get(word, None):
+            rebuilt_spam_vocabulary_probabilities[word] += 1
+        else:
+            rebuilt_spam_vocabulary_probabilities[word] = 1
+
+for key in rebuilt_spam_vocabulary_probabilities:
+    rebuilt_spam_vocabulary_probabilities[key] = (rebuilt_spam_vocabulary_probabilities[key] + 1) / (len(spam_training) + len(master_vocabulary))
+
+
+for item in ham_test:
+    print(item['[[file_path]]'])
+    ham_probability = len(ham_training) / (len(ham_training) + len(ham_test))
+    spam_probability = len(spam_training) / (len(spam_training) + len(spam_test))
+    for word in item.keys():
+        ham_probability *= rebuilt_ham_vocabulary_probabilities.get(word, 1 / (len(ham_training) * 2 + len(spam_training)))
+        spam_probability *= rebuilt_spam_vocabulary_probabilities.get(word, 1 / (len(spam_training) * 2 + len(spam_training)))
+
+    if ham_probability > spam_probability:
+        print('HAM!')
+    elif spam_probability > ham_probability:
+        print('SPAM!')
+
+
+for item in spam_test:
+    print(item['[[file_path]]'])
+    ham_probability = len(ham_training) / (len(ham_training) + len(ham_test))
+    spam_probability = len(spam_training) / (len(spam_training) + len(spam_test))
+    for word in item.keys():
+        ham_probability *= rebuilt_ham_vocabulary_probabilities.get(word, 1 / (len(ham_training) * 2 + len(spam_training)))
+        spam_probability *= rebuilt_spam_vocabulary_probabilities.get(word, 1 / (len(spam_training) * 2 + len(spam_training)))
+
+    if ham_probability > spam_probability:
+        print('HAM!')
+    else spam_probability > ham_probability:
+        print('SPAM!')
